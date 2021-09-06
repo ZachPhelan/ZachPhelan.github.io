969719836103dbc776678cddb2086fc91fb500ba
ZachPhelan <ZachCPhelan@gmail.com>
2021-09-06 17:39:35 -0600

    Trying to stringify output out of JSON

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -6,9 +6,9 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
   var suggestions = writeGood('So the cat was stolen.');
-  core.setOutput("out", suggestions);
+  var output = JSON.stringify(suggestions, undefined, 2);
+  core.setOutput("out", output);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
